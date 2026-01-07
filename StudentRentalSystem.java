9b8d46bd084fd86bdc2a7dc07b9408c4d2a214cb
Geth-Ev <gethingino@gmail.com>
2026-01-07 22:29:33 +0000

    Added method to get properties from a homeowner, as well as adding and removing properties

--- StudentRentalSystem.java
+++ StudentRentalSystem.java
@@ -4,14 +4,38 @@ import java.util.ArrayList;
 public class StudentRentalSystem {
     private List<User> users;
     private User currentUser;
+    private List<Property> properties;
 
     public StudentRentalSystem() {
         this.users = new ArrayList<>();
         this.currentUser = null;
+        this.properties = new ArrayList<>();
     }
 
     public void registerUser(User user) { // Adds user to the system
         users.add(user);
+    }
+
+    public void addProperty(Property property) { // Adds property to the system
+        properties.add(property);
+    }
+
+    public void removeProperty(Property property) { // Removes property from the system
+        properties.remove(property);
+    }
+
+    public List<Property> getAllProperties() { // Returns all properties
+        return properties;
+    }
+
+    public List<Property> getPropertiesByOwner(Homeowner homeowner) { // Returns properties owned by specified homeowner
+        List<Property> ownerProperties = new ArrayList<>();
+        for (Property property : properties) {
+            if (property.getOwner() != null && property.getOwner().equals(homeowner)) {
+                ownerProperties.add(property);
+            }
+        }
+        return ownerProperties;
     }
 
     public boolean login(String email, String password) { // Authenticates user credentials
