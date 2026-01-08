956afbf217f276c96573c295726cd59fe2b2f4ca
Geth-Ev <gethingino@gmail.com>
2026-01-08 05:08:30 +0000

    Changed amenities to String rather than List<String>

--- Room.java
+++ Room.java
@@ -6,12 +6,13 @@ public class Room {
     private int id;
     private double price;
     private RoomType type;
-    private List<String> amenities = new ArrayList<>();
+    private String amenities;
     private List<DateRange> availability = new ArrayList<>();
     private List<DateRange> bookedDates = new ArrayList<>();
     private Property property;
     
-    public Room(double price, RoomType type, List<String> amenities, List<DateRange> bookedDates) {
+    public Room(int id, double price, RoomType type, String amenities, List<DateRange> bookedDates, Property property) {
+        this.id = id;
         this.price = price;
         this.type = type;
         this.amenities = amenities;
@@ -41,11 +42,14 @@ public class Room {
     public void removeBooking(LocalDate start, LocalDate end) {
         DateRange bookingRange = new DateRange(start, end);
         if (bookedDates != null) {
-            bookedDates.remove(bookingRange); //change
+            bookedDates.remove(bookingRange);
         }
     }
 
     public Property getProperty() { return property; }
     public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public double getPrice() { return price; }
+    public RoomType getType() { return type; }
+    public String getAmenities() { return amenities; }
+    public List<DateRange> getBookedDates() { return bookedDates; }
 }
