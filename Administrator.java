7970e984a4a013d6492c032502b218d3d850a9ed
Geth-Ev <gethingino@gmail.com>
2026-01-08 21:31:11 +0000

    Added comment

--- Administrator.java
+++ Administrator.java
@@ -1,4 +1,4 @@
-public class Administrator extends User {
+public class Administrator extends User { // Potentially useful if I choose to implement 3.6
     public Administrator(int id, String name, String email, String password, Role role) {
         super(id, name, email, password, role);
     }
